f0b8dc9eda1bfe5cb932f0dd962598984674ed26
jakariamasum <jmphy698@gmail.com>
2023-05-11 19:07:51 +0600

    authcontext added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,42 +1,57 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/login/login.svg'
-import { FaFacebookSquare,FaGoogle,FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookSquare, FaGoogle, FaLinkedinIn } from "react-icons/fa";
+import { AuthContext } from '../../Provider/AuthProvider';
+import { useContext } from 'react';
 
 const Login = () => {
+    const {user}=useContext(AuthContext)
+    console.log(user)
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
                 <div className="text-center lg:text-left">
-                <img src={logo} alt="" />
+                    <img src={logo} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border border-[#D0D0D0]">
-                    <div className="card-body p-16">
+                    <div className="card-body p-16" >
                         <h1 className="font-bold text-4xl text-center">Login</h1>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text font-bold">Email</span>
-                            </label>
-                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text font-bold">Password</span>
-                            </label>
-                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary bg-[#FF3811] border-none">Login</button>
-                        </div>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input type="submit" value="Login"  className='btn btn-primary bg-[#FF3811] border-none'/>
+                            </div>
+                        </form>
                         <div className='text-center'>
                             <p className='font-semibold my-7'>Or Sign In With</p>
                             <div className='flex gap-2 justify-center'>
-                                <FaFacebookSquare className='text-blue-500 '/>
-                                <FaLinkedinIn className='text-blue-500 '/>
-                                <FaGoogle className='text-[#0F9D58] '/>
+                                <FaFacebookSquare className='text-blue-500 ' />
+                                <FaLinkedinIn className='text-blue-500 ' />
+                                <FaGoogle className='text-[#0F9D58] ' />
                             </div>
-                            <p className="text-center mt-2">Have an account? <Link  to='/register'><span className='link text-[#FF3811]'>Sign In</span></Link> </p>
+                            <p className="text-center mt-2">Have an account? <Link to='/register'><span className='link text-[#FF3811]'>Sign In</span></Link> </p>
                         </div>
                     </div>
                 </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,8 +1,16 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/login/login.svg'
-import { FaFacebookSquare,FaGoogle,FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookSquare, FaGoogle, FaLinkedinIn } from "react-icons/fa";
 
 const Register = () => {
+    const handleRegister=(event)=>{
+        event.preventDefault(); 
+        const form=event.target; 
+        const name=form.name.value;
+        const email=form.email.value;
+        const password=form.password.value;
+        console.log(name,email,password)
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
@@ -12,29 +20,31 @@ const Register = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border border-[#D0D0D0]">
                     <div className="card-body p-16">
                         <h1 className="font-bold text-4xl text-center">Sign Up</h1>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text font-bold">Name</span>
-                            </label>
-                            <input type="text" name='name' placeholder="name" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text font-bold">Email</span>
-                            </label>
-                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text font-bold">Confirm Password</span>
-                            </label>
-                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary bg-[#FF3811] border-none">Sign Up</button>
-                        </div>
+                        <form onSubmit={handleRegister}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Email</span>
+                                </label>
+                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Confirm Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input type="submit" value="Sign Up" className="btn btn-primary bg-[#FF3811] border-none" />
+                            </div>
+                        </form>
                         <div className='text-center'>
                             <p className='font-semibold my-7'>Or Sign Up With</p>
                             <div className='flex gap-2 justify-center'>
